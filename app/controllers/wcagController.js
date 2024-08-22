e22cb37e736115e64cacfee6c7ad77e8cf906d72
Andy Jones <andy.jones@digital.education.gov.uk>
2024-08-22 13:31:42 +0100

    Adding aka to all pages

--- app/controllers/wcagController.js
+++ app/controllers/wcagController.js
@@ -7,3 +7,12 @@ exports.g_wcag = async function (req, res) {
 
     return res.render('wcag/index')
 }
+
+
+exports.g_wcagCriteria = async function (req, res) {
+
+    // Get the criterion from the wcag file
+    const wcag = require('../data/wcag.json')   
+    
+    return res.render('knowledge-hub/wcag/criteria/index', { wcag })
+    }
