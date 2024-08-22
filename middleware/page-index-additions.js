1076dbdc9737e454eacb3874566bc4e6d3d5d163
Andy Jones <andy.jones@digital.education.gov.uk>
2024-08-22 12:33:21 +0100

    indicies

--- middleware/page-index-additions.js
+++ middleware/page-index-additions.js
@@ -1,6 +1,8 @@
 /* eslint-disable quote-props */
 // Page specific
 const additionalIndices = {
+  '/training/accessibility-inclusion': ['101'],
+  '/knowledge-hub/wcag': ['2.1, 2.2, 21, 22, wgac'],
 };
 
 // Term specific
