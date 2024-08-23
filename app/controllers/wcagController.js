e85b1bbf7d305c48132f183dbaec08318fde04bc
Andy Jones <andy.jones@digital.education.gov.uk>
2024-08-23 10:55:04 +0100

    WCAG criteria

--- app/controllers/wcagController.js
+++ app/controllers/wcagController.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 exports.g_wcag = async function (req, res) {
 
-    
+
 
     return res.render('wcag/index')
 }
@@ -12,7 +12,22 @@ exports.g_wcag = async function (req, res) {
 exports.g_wcagCriteria = async function (req, res) {
 
     // Get the criterion from the wcag file
-    const wcag = require('../data/wcag.json')   
-    
-    return res.render('knowledge-hub/wcag/criteria/index', { wcag })
-    }
+    const wcag = require('../data/wcag.json')
+    const groupedCriteria = groupCriteriaByPrinciple(wcag);
+
+
+    return res.render('knowledge-hub/wcag/criteria/index', { wcag: groupedCriteria })
+}
+
+
+
+const groupCriteriaByPrinciple = (criteriaArray) => {
+    return criteriaArray.reduce((accumulator, current) => {
+        const principle = current.principle;
+        if (!accumulator[principle]) {
+            accumulator[principle] = [];
+        }
+        accumulator[principle].push(current);
+        return accumulator;
+    }, {});
+};
